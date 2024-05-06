026b1f25c46d7ad77230cc5409de9e8746a59c34
timphilip12 <148401261+timphilip12@users.noreply.github.com>
2024-05-05 23:41:49 -0500

    Add files via upload

---
+++ Uber/uber_assignment.R
@@ -0,0 +1,469 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(lubridate)
+library(leaflet)
+library(leaflet.extras)
+library(data.table)
+library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(stringr)
+
+
+# save into rds
+rm(list = ls())
+# setwd('C:/Users/timot/OneDrive/Augustana/Junior year/Spring semester/DATA 332/Uber')
+
+combined_uber <- readRDS("combined_uber.rds")
+
+# raw_uber_apr <- read.csv("uber-raw-data-apr14.csv")
+# 
+# raw_uber_may <- read.csv("uber-raw-data-may14.csv")
+# 
+# raw_uber_jun <- read.csv("uber-raw-data-jun14.csv")
+# 
+# raw_uber_jul <- read.csv("uber-raw-data-jul14.csv")
+# 
+# raw_uber_aug <- read.csv("uber-raw-data-aug14.csv")
+# 
+# raw_uber_sep <- read.csv("uber-raw-data-sep14.csv")
+# 
+# combined_uber <- bind_rows(raw_uber_apr, raw_uber_may, raw_uber_jun, raw_uber_jul, raw_uber_aug, raw_uber_sep)
+# 
+# combined_uber$Date.Time <- as.POSIXct(combined_uber$Date.Time, format = "%m/%d/%Y %H:%M:%S")
+# 
+# combined_uber <- combined_uber %>%
+#   mutate(hour = format(Date.Time, "%H"))
+# 
+# combined_uber <- combined_uber %>%
+#   mutate(Month = format(Date.Time, "%m"))
+# 
+# combined_uber <- combined_uber %>%
+#   mutate(Day = format(Date.Time, "%d"))
+# 
+# combined_uber$MonthName <- month.name[as.integer(combined_uber$Month)]
+# 
+# combined_uber$Month <- paste(combined_uber$Month, combined_uber$MonthName, sep = " ")
+# 
+# combined_uber$MonthName <- NULL
+# 
+# combined_uber$DayOfWeek <- weekdays(combined_uber$Date.Time)
+# 
+# combined_uber$week_number <- week(combined_uber$Date.Time)
+# 
+# combined_uber$Base <- as.factor(combined_uber$Base)
+# 
+# saveRDS(combined_uber, "combined_uber.rds")
+
+leaflet_data <- combined_uber %>%
+  sample_n(1000)
+
+pivot_hour <- combined_uber %>%
+  group_by(hour) %>%
+  summarise(count = n()) %>%
+  arrange(hour)
+
+pivot_hour_month <- combined_uber %>%
+  group_by(hour, Month) %>%
+  summarize(count = n())
+
+pivot_day <- combined_uber %>%
+  group_by(Day) %>%
+  summarize(count = n())
+
+pivot_day_of_week <- combined_uber %>%
+  group_by(Month, DayOfWeek) %>%
+  summarize(count = n())
+
+pivot_hour_day <- combined_uber %>%
+  group_by(hour, Day) %>%
+  summarize(count = n())
+
+day_order <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+
+pivot_day_of_week$DayOfWeek <- factor(pivot_day_of_week$DayOfWeek, levels = day_order)
+
+pivot_hour_DayOfWeek <- combined_uber %>%
+  group_by(hour, DayOfWeek) %>%
+  summarize(count = n())
+
+pivot_base <- combined_uber %>%
+  group_by(Base, Month) %>%
+  summarize(count = n())
+
+pivot_month_day <- combined_uber %>%
+  group_by(Month, Day) %>%
+  summarize(count = n())
+
+pivot_month_week <- combined_uber %>%
+  group_by(Month, week_number) %>%
+  summarize(count = n())
+
+pivot_base_DayOfWeek <- combined_uber %>%
+  group_by(Base, DayOfWeek) %>%
+  summarize(count = n())
+
+pivot_base_DayOfWeek$DayOfWeek <- factor(pivot_base_DayOfWeek$DayOfWeek, levels = day_order)
+
+pivot_month <- combined_uber %>%
+  group_by(Month) %>%
+  summarize(count = n())
+
+ggplot(pivot_hour_month, aes(x = hour, y = count, fill = Month)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Hourly Uber Rides by Month",
+       x = "Hour of the Day",
+       y = "Number of rides",
+       color = "Month") +
+  theme_minimal()
+
+
+ggplot(pivot_hour, aes(x = hour, y = count)) +
+  geom_bar(stat = "identity", fill = "#4CAF50") +
+  labs(title = "Hourly Uber",
+       x = "Hour of the Day",
+       y = "Number of rides") +
+  theme_minimal()
+
+ggplot(pivot_day, aes(x = Day, y = count)) +
+  geom_bar(stat = "identity", fill = "blue") +
+  labs(title = "Daily Uber",
+       x = "Day of the month",
+       y = "Number of rides") +
+  theme_minimal()
+
+
+ggplot(pivot_day_of_week, aes(x = Month, y = count, fill = DayOfWeek)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Uber Rides by Month and by weekdays",
+       x = "Month",
+       y = "Number of rides",
+       color = "Day of the Week") +
+  theme_minimal()
+
+ggplot(pivot_base, aes(x = Base, y = count, fill = Month)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Uber Rides by Base and by Month",
+       x = "Base",
+       y = "Number of rides",
+       color = "Month") +
+  theme_minimal()
+
+
+
+ggplot(pivot_hour_day, aes(x = hour, y = Day, fill = count)) +
+  geom_tile() +
+  labs(title = "Hour Day Heatmap",
+       x = "Hour of the day",
+       y = "Days")
+
+ggplot(pivot_month_day, aes(x = Month, y = Day, fill = count)) +
+  geom_tile() +
+  labs(title = "Hour Day Heatmap",
+       x = "Months",
+       y = "Days")
+
+ggplot(pivot_month_week, aes(x = Month, y = week_number, fill = count)) +
+  geom_tile() +
+  labs(title = "Hour Week Heatmap",
+       x = "Month",
+       y = "Week number")
+
+ggplot(pivot_base_DayOfWeek, aes(x = Base, y = DayOfWeek, fill = count)) +
+  geom_tile() +
+  labs(title = "Base DayOfWeek Heatmap",
+       x = "Base",
+       y = "Day of the Week")
+
+
+leaflet() %>%
+  addTiles() %>%  
+  addMarkers(
+    data = leaflet_data,
+    lat = ~Lat,
+    lng = ~Lon
+  )
+
+
+
+rides_per_day <- combined_uber %>%
+  group_by(Day) %>%
+  summarise(rides = n()/6)
+
+rides_per_day$Day <- as.numeric(as.character(rides_per_day$Day))
+
+lm_model_day <- lm(rides_per_day$rides ~ rides_per_day$Day)
+
+# Extract slope and intercept
+intercept_day <- coef(lm_model_day)[1]
+slope_day <- coef(lm_model_day)[2]
+
+# Plot scatter plot
+ggplot(rides_per_day, aes(x = Day, y = rides)) +
+  geom_point() + # Scatter plot
+  geom_abline(intercept = intercept_day, slope = slope_day, color = "red") +  # Add regression line
+  labs(title = "Number of Rides per Day", x = "Day", y = "Number of Rides")
+
+
+
+rides_per_month <- combined_uber %>%
+  group_by(Month) %>%
+  summarise(rides = n()/6)
+
+rides_per_month <- rides_per_month %>%
+  separate(Month, into = c("Month_Number", "Month_Name"), sep = " ")
+
+rides_per_month <- rides_per_month %>%
+  mutate(Month_Number = as.integer(Month_Number))
+
+rides_per_month$Month_Number <- as.numeric(as.character(rides_per_month$Month_Number))
+
+lm_model_month <- lm(rides_per_month$rides ~ rides_per_month$Month_Number)
+
+# Extract slope and intercept
+intercept_month <- coef(lm_model_month)[1]
+slope_month <- coef(lm_model_month)[2]
+
+
+# Plot scatter plot
+ggplot(rides_per_month, aes(x = Month_Number, y = rides)) +
+  geom_point() + # Scatter plot
+  geom_abline(intercept = intercept_month, slope = slope_month, color = "red") +
+  labs(title = "Number of Rides per Month", x = "Month", y = "Number of Rides")
+
+
+ui <- dashboardPage(
+  skin = "purple",
+  dashboardHeader(title = "Data Visualization"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Bar Chart", tabName = "bar_chart", icon = icon("bar-chart")),
+      menuItem("Heatmap", tabName = "heatmap", icon = icon("heatmap")),
+      menuItem("Leaflet Map", tabName = "map", icon = icon("map")),
+      menuItem("Predictive chart", tabName = "predictive_chart", icon = icon("predictive"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(
+        tabName = "bar_chart",
+        fluidRow(
+          column(12, plotOutput("bar_chart1")),
+          column(12, verbatimTextOutput("bar_chart1_desc"))
+        ),
+        fluidRow(
+          column(12, plotOutput("bar_chart2")),
+          column(12, verbatimTextOutput("bar_chart2_desc"))
+        ),
+        fluidRow(
+          column(12, plotOutput("bar_chart3")),
+          column(12, verbatimTextOutput("bar_chart3_desc"))
+        ),
+        fluidRow(
+          column(12, plotOutput("bar_chart4")),
+          column(12, verbatimTextOutput("bar_chart4_desc"))
+        ),
+        fluidRow(
+          column(12, plotOutput("bar_chart5")),
+          column(12, verbatimTextOutput("bar_chart5_desc"))
+        )
+      ),
+      tabItem(
+        tabName = "heatmap",
+        fluidRow(
+          column(12, plotOutput("heatmap1")),
+          column(12, verbatimTextOutput("heatmap1_desc"))
+        ),
+        fluidRow(
+          column(12, plotOutput("heatmap2")),
+          column(12, verbatimTextOutput("heatmap2_desc"))
+        ),
+        fluidRow(
+          column(12, plotOutput("heatmap3")),
+          column(12, verbatimTextOutput("heatmap3_desc"))
+        ),
+        fluidRow(
+          column(12, plotOutput("heatmap4")),
+          column(12, verbatimTextOutput("heatmap4_desc"))
+        )
+      ),
+      tabItem(
+        tabName = "map",
+        leafletOutput("map"),
+        verbatimTextOutput("map_desc")
+      ),
+      tabItem(
+        tabName = "predictive_chart",
+        fluidRow(
+          column(12, plotOutput("predictive_chart1")),
+          column(12, verbatimTextOutput("predictive_chart1_desc"))
+        ),
+        fluidRow(
+          column(12, plotOutput("predictive_chart2")),
+          column(12, verbatimTextOutput("predictive_chart2_desc"))
+        )
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$bar_chart1 <- renderPlot({
+    ggplot(pivot_hour_month, aes(x = hour, y = count, fill = Month)) +
+      geom_bar(stat = "identity", position = "dodge") +
+      labs(title = "Hourly Uber Rides by Month",
+           x = "Hour of the Day",
+           y = "Number of rides",
+           color = "Month") +
+      theme_minimal()
+  })
+  output$bar_chart1_desc <- renderText({
+    "Description: This bar chart displays the amount of uber ride per hour of the day for each month. We can see that the amount of rides significantly increases from April to September"
+  })
+  
+  output$bar_chart2 <- renderPlot({
+    ggplot(pivot_hour, aes(x = hour, y = count)) +
+      geom_bar(stat = "identity", fill = "#4CAF50") +
+      labs(title = "Hourly Uber",
+           x = "Hour of the Day",
+           y = "Number of rides") +
+      theme_minimal()
+  })
+  output$bar_chart2_desc <- renderText({
+    "Description: This bar chart displays the amount of rides for every hour of the day in military time. The time with the most rides is 17 (5pm) and with the least rides is 2 (2am) which makes sense."
+  })
+  
+  output$bar_chart3 <- renderPlot({
+    ggplot(pivot_day, aes(x = Day, y = count)) +
+      geom_bar(stat = "identity", fill = "blue") +
+      labs(title = "Daily Uber",
+           x = "Day of the month",
+           y = "Number of rides") +
+      theme_minimal()
+  })
+  output$bar_chart3_desc <- renderText({
+    "Description: This bar chart displays the amount of rides for every days of the month. This graph is pretty consistant appart from the 31st which is way lower but it makes sense since only half of the month in this data have 31 days (May, July and August)"
+  })
+  
+  output$bar_chart4 <- renderPlot({
+    ggplot(pivot_day_of_week, aes(x = Month, y = count, fill = DayOfWeek)) +
+      geom_bar(stat = "identity", position = "dodge") +
+      labs(title = "Uber Rides by Month and by weekdays",
+           x = "Month",
+           y = "Number of rides",
+           color = "Day of the Week") +
+      theme_minimal()
+  })
+  output$bar_chart4_desc <- renderText({
+    "Description: This bar chart displays the amount of rides per month for each day of the week. There is not a particular day of the week that has more rides than all the other days but Sunday is definitely the day with the least rides. Also, we can notice that the overall amount of rides increases from April to September"
+  })
+  
+  output$bar_chart5 <- renderPlot({
+    ggplot(pivot_base, aes(x = Base, y = count, fill = Month)) +
+      geom_bar(stat = "identity", position = "dodge") +
+      labs(title = "Uber Rides by Base and by Month",
+           x = "Base",
+           y = "Number of rides",
+           color = "Month") +
+      theme_minimal()
+  })
+  output$bar_chart5_desc <- renderText({
+    "Description: This bar chart displays the amount of rides per base for each month. We can see that three bases share most of the rides and that the two other bases have significantly less rides."
+  })
+  
+  
+  # Render heatmap
+  output$heatmap1 <- renderPlot({
+    ggplot(pivot_hour_day, aes(x = hour, y = Day, fill = count)) +
+      geom_tile() +
+      labs(title = "Hour Day Heatmap",
+           x = "Hour of the day",
+           y = "Days")
+  })
+  output$heatmap1_desc <- renderText({
+    "Description: This heatmap displays the amount of rides by hour of the day and days. There is no days that have a big difference of rides appart from the 31st but it is because only half of the month in this data have 31 days. We can notice that the times with the most rides are at the end of the afternoon and the times with the least rides are in the middle of the night."
+  })
+  
+  output$heatmap2 <- renderPlot({
+    ggplot(pivot_month_day, aes(x = Month, y = Day, fill = count)) +
+      geom_tile() +
+      labs(title = "Hour Day Heatmap",
+           x = "Months",
+           y = "Days")
+  })
+  output$heatmap2_desc <- renderText({
+    "Description: This heatmap displays the amount of rides by Months and days of the month. We can see kind of a patern which probably due to the fact that there are more rides around the end of the week. We can also see that the heatmap gets clearer from April to September which means that there are more and more rides."
+  })
+  
+  output$heatmap3 <- renderPlot({
+    ggplot(pivot_month_week, aes(x = Month, y = week_number, fill = count)) +
+      geom_tile() +
+      labs(title = "Hour Week Heatmap",
+           x = "Month",
+           y = "Week number")
+  })
+  output$heatmap3_desc <- renderText({
+    "Description: This heatmap displays the amount of rides by Months and week number. We can't really say anything from this graph except the fact that there are more and more rides from April to September. Also, there are some very dark spots but it is only because the weeks is over two different months"
+  })
+  
+  output$heatmap4 <- renderPlot({
+    ggplot(pivot_base_DayOfWeek, aes(x = Base, y = DayOfWeek, fill = count)) +
+      geom_tile() +
+      labs(title = "Base DayOfWeek Heatmap",
+           x = "Base",
+           y = "Day of the Week")
+  })
+  output$heatmap4_desc <- renderText({
+    "Description: This heatmap displays the amount of rides by Base and days of the week. We can see that there are three bases that have way more rides that the two last bases. Also, Thurdays and Fridays are clearer that the rest of the days and Sundays are darker that the rest of the days."
+  })
+  
+  # Render Leaflet map
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%  
+      addMarkers(
+        data = leaflet_data,
+        lat = ~Lat,
+        lng = ~Lon
+      )
+  })
+  output$map_desc <- renderText({
+    "Description: This map only displays a sample of 1000 rows out the 4.5 millions rows of the dataset. I couldn't run it with 4.5 millions rows because it was too big for my laptop. I then decided to take a random sample of 1000 rows so I have a map to show."
+  })
+  
+  output$predictive_chart1 <- renderPlot({
+    ggplot(rides_per_month, aes(x = Month_Number, y = rides)) +
+      geom_point() + # Scatter plot
+      geom_abline(intercept = intercept_month, slope = slope_month, color = "red") +
+      labs(title = "Average number of Rides per Month", x = "Month", y = "Number of Rides")
+  })
+  
+  output$predictive_chart1_desc <- renderText({
+    "Description: This is a scatter plot with a regression line that predicts how the number of rides will evoluate when grouped by month. We can see that the slope of the regression line is positive which means that the number of rides will probably grow in the future months."
+  })
+  
+  output$predictive_chart2 <- renderPlot({
+    ggplot(rides_per_day, aes(x = Day, y = rides)) +
+      geom_point() + # Scatter plot
+      geom_abline(intercept = intercept_day, slope = slope_day, color = "red") +  # Add regression line
+      labs(title = "average number of Rides per Day of the Month", x = "Day", y = "Number of Rides")
+  })
+  
+  output$predictive_chart2_desc <- renderText({
+    "Description: This is scatter plot with a regression line that predicts how the number of rides will evoluate when grouped by day of the month. We can see that the slope is slightly negative which means that people are using uber a little bit more in the first days of the month. However, this is affected by the facts that only half of the month in this data have 31 days."
+  })
+  
+  
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
